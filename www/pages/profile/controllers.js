3db5a956e3eac168024c1adae871fa8d032257bb
cusackd <cusackd@gmail.com>
2016-02-21 20:32:58 +0000

    Fixes

--- www/pages/profile/controllers.js
+++ www/pages/profile/controllers.js
@@ -13,10 +13,31 @@ var profileControllers = angular.module('profileControllers', [])
 
     $scope.saveDetails = function(){
         var name = this.username;
-        console.log(name);
-        addToLocalStorage(name);
 
-        $cordovaGoogleAnalytics.trackEvent('Profile', 'Save', 'Username');
+
+        if(validateName(name)){
+          addToLocalStorage(name);
+          document.addEventListener("deviceready", function () {
+            $cordovaGoogleAnalytics.trackEvent('Profile', 'Save', 'Success');
+          });
+        }else{
+          alert('Please type a name before saving');
+          document.addEventListener("deviceready", function () {
+            $cordovaGoogleAnalytics.trackEvent('Profile', 'Save', 'Failure');
+          });
+        }
+
+
+    }
+
+    validateName = function(name){
+
+      if(name.length < 1){
+        return false;
+      }
+
+      return true;
+
     }
 
     getLocalStorage = function(){
